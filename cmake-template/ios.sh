3f0fdf84bff31130781d8b0439c7ce92867ed879
guo122 <balanuard@163.com>
2019-10-25 11:06:19 +0800

    [cmake_template] add open 3rd solution.

--- cmake-template/ios.sh
+++ cmake-template/ios.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+cd "$(dirname "$0")"
+
 cmakeImpl() {
     cmake -E make_directory "Solution/iOS" && cmake -E chdir "Solution/iOS" cmake -G "Xcode" ../../ -DCMAKE_TOOLCHAIN_FILE=./BuildScript/cmake/ios-cmake/ios.toolchain.cmake -DPLATFORM=OS64COMBINED
+	cmake_ret="$?"
 }
 
 cmakeImpl
 
-if [ "$?" == 0 ] ; then
+if [ "$cmake_ret" == 0 ] ; then
     open ./Solution/iOS/*.xcodeproj 2>/dev/null
+fi
+
+if [ "$1" == "a" -a -f "./script/open3rdSolution_ios.sh" ] ; then
+    ./script/open3rdSolution_ios.sh
 fi
--- cmake-template/macos.sh
+++ cmake-template/macos.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+cd "$(dirname "$0")"
+
 cmakeImpl() {
 	cmake -E make_directory "Solution/macOS" && cmake -E chdir "Solution/macOS" cmake -G "Xcode" ../../
+	cmake_ret="$?"
 }
 
 cmakeImpl
 
-if [ "$?" == 0 ] ; then
+if [ "$cmake_ret" == 0 ] ; then
 	open ./Solution/macOS/*.xcodeproj 2>/dev/null
+fi
+
+if [ "$1" == "a" -a -f "./script/open3rdSolution_macos.sh" ] ; then
+	./script/open3rdSolution_macos.sh
 fi
